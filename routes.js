8908b375796b879687f473692b2851ff814de936
Anubhav Choudhary <ac.10edu@gmail.com>
2022-01-21 23:31:10 +0530

    [func] create route
    Signed-off-by: Anubhav Choudhary <ac.10edu@gmail.com>

--- routes.js
+++ routes.js
@@ -3,32 +3,49 @@ const { unlink } = require("fs");
 const multer = require("multer");
 
 const fileParser = require("./fileParser");
+const People = require("./schema/sampleSchema");
 
 const router = express();
 const upload = multer({ dest: "public/upload/" });
-const People = require("./schema/sampleSchema");
+router.use(upload.array());
 
 router.use(express.json());
 
+// Send CSV to DB
 router.post("/upload", upload.single("csvFile"), async (req, res) => {
   const { path: filePath } = req.file;
-  var parsedObject = await fileParser(filePath);
+  const parsedObject = await fileParser(filePath);
 
-  People.create(parsedObject, (err) => {
-    if (err) throw err;
-    else {
+  People.create(parsedObject)
+    .then(() => {
       console.log("Data sent to DB");
       unlink(filePath, (err) => {
         if (err) throw err;
         console.log("Deleted raw CSV file");
       });
-    }
-  });
-
-  res.json(parsedObject);
+      res.json(parsedObject);
+    })
+    .catch((err) => {
+      res.sendStatus(501);
+      throw err;
+    });
 });
 
 // Create
+router.post("/create", (req, res) => {
+  const data = new People(req.body);
+  data
+    .save()
+    .then(() => {
+      res.sendStatus(201);
+      console.log("New doc added to DB");
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+      throw err;
+    });
+});
+
 // Read
 // Update
 // Delete
